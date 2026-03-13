b11f2e0e3327d9f86bdb9bbbee66f75bb51b4e04
EI <darlinshot@gmail.com>
2026-03-13 18:51:22 +0700

    Add DefaultXController in case. Reformat Character class.

--- src/ServerScriptService/Main.server.lua
+++ src/ServerScriptService/Main.server.lua
@@ -6,5 +6,11 @@ local SHAREDMODULES = rs.SharedModules
 local Character = require(SHAREDMODULES.Character)
 Character.Init()
 
-local char = Character.New()
-char:Update()
+for i = 100, 1, -1 do
+    for j = 10, 1, -1 do
+		local char = Character.New()
+        char.Model:PivotTo(CFrame.new(i*6, 30, j*6))
+		char:Update()
+       
+    end
+end
