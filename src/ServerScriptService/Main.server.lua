2cfa1984e72aca172754906db7a1b31e961a4e96
EI <darlinshot@gmail.com>
2026-03-13 19:05:23 +0700

    Add CachedFloorResult to Character for EnvironmentState.

--- src/ServerScriptService/Main.server.lua
+++ src/ServerScriptService/Main.server.lua
@@ -6,8 +6,8 @@ local SHAREDMODULES = rs.SharedModules
 local Character = require(SHAREDMODULES.Character)
 Character.Init()
 
-for i = 100, 1, -1 do
-    for j = 10, 1, -1 do
+for i = 1, 1, -1 do
+    for j = 1, 1, -1 do
 		local char = Character.New()
         char.Model:PivotTo(CFrame.new(i*6, 30, j*6))
 		char:Update()
