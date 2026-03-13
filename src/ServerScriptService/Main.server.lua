8392adedb6d318470da46b50d45814188a09d702
EI <darlinshot@gmail.com>
2026-03-13 16:09:18 +0700

    Add Environment flagging. Fix ActiveControllers.

--- src/ServerScriptService/Main.server.lua
+++ src/ServerScriptService/Main.server.lua
@@ -5,5 +5,6 @@ local rs = game:GetService("ReplicatedStorage")
 local SHAREDMODULES = rs.SharedModules
 local Character = require(SHAREDMODULES.Character)
 Character.Init()
+
 local char = Character.New()
 char:Update()
